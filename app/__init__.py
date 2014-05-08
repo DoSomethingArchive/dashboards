63e115683b860476c23649109c17351691f368a9
Andrea Gaither <angaither@gmail.com>
2014-05-08 19:33:42 -0400

    Initial commit, restructure direct from other repo.

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import MySQLdb
+import MySQLdb.converters
+import MySQLdb.cursors
+from MySQLdb.constants import FIELD_TYPE
+from random import choice as choice
+import json
+
+app = Flask(__name__)
+
+app.config.from_pyfile('../env/config.cfg')
+my_conv = { FIELD_TYPE.LONG: int }
+
+def openDB():
+  db = MySQLdb.connect(host=app.config['HOST'], #hostname
+                    user=app.config['USER'], # username
+                    passwd=app.config['PW'], # password
+                    db=app.config['DB'], # db
+                    conv=my_conv,# datatype conversions
+                    cursorclass=MySQLdb.cursors.DictCursor)
+  cur = db.cursor()
+  return cur
+
+
+import views
