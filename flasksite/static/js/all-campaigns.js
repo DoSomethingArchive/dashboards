508cff669a0d3f3d30f206e3c9f7f241045e6232
Andrea Gaither <angaither@gmail.com>
2014-05-13 14:19:10 -0400

    Style, tabs, etc

--- flasksite/static/js/all-campaigns.js
+++ flasksite/static/js/all-campaigns.js
@@ -1,6 +1,7 @@
 d3.json('/get-all-campaigns-data.json', function(error, json) {
   if (error) return console.warn(error);
   data = json;
+
   var margin = {top: 20, right: 20, bottom: 30, left: 40},
       width = 1100 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
@@ -25,25 +26,28 @@ d3.json('/get-all-campaigns-data.json', function(error, json) {
       .orient("left")
       .tickFormat(d3.format("2s"));
 
-
-
   var svg = d3.select("body").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .attr("id","svgMain");
-    /*returns an object array with all ages and state, so '5 and under':100, '16':150, 'state':ca, and each array is a state. This is how d3 converts the csv. The remaining code creates an array of keys, and then foreach array in the data (d3.foreach(data)), it creates a new ages  object array for each state, each satae  has an ages array each element og the array being an age:value object. It then creates the x0 domain with d.State from data using maop, passes thr x range to x1, and uses max age for y. The g elements are based on the x0(state) scale. Finally, for each g, rects are built from each states ages array, the name being the x, and height the value. Try this example by recreating it with the data object array data structure (the original). If that works, chnage the data to ds data
 
-
-
+    /** Return an object array with all ages and state, so '5 and under':100, '16':150, 'state':ca,
+    * and each array is a state. This is how d3 converts the csv.
+    * The remaining code creates an array of keys, and then foreach array in the data (d3.foreach(data)),
+    * it creates a new ages  object array for each state, each satae  has an ages array each element
+    * the array being an age:value object.
+    * It then creates the x0 domain with d.State from data using maop, passes thr x range to x1,
+    * and uses max age for y. The g elements are based on the x0(state) scale.
+    * Finally, for each g, rects are built from each states ages array, the name being the x, and height the value.
+    * Try this example by recreating it with the data object array data structure (the original).
+    * If that works, chnage the data to ds data
     */
+
     var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "campaign"; });
 
     data.forEach(function(d) {
       d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
-
       d.ages.sort(function(a, b){ return d3.descending(a.value, b.value); });
-
-
     });
 
     x0.domain(data.map(function(d) { return d.campaign; }));
@@ -56,8 +60,7 @@ d3.json('/get-all-campaigns-data.json', function(error, json) {
 
   var yAxis = d3.svg.axis()
       .scale(y)
-      .orient("left")
-
+      .orient("left");
 
   svg.append("g")
         .attr("class", "x axis")
@@ -67,8 +70,7 @@ d3.json('/get-all-campaigns-data.json', function(error, json) {
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-
-      .append("text")
+        .append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
         .attr("dy", ".71em")
@@ -77,67 +79,55 @@ d3.json('/get-all-campaigns-data.json', function(error, json) {
 
     var campaign = svg.selectAll(".campaign")
         .data(data)
-
-      .enter().append("g")
+        .enter().append("g")
         .attr("class", "g")
         .attr("transform", function(d) { return "translate(" + x0(d.campaign) + ",0)";});
-  //set y to height
+
+    function showTooltip(d) {
+      // Get this bar's x/y values, then augment for the tooltip
+      var xPosition = parseFloat(d3.select(this).attr("x")) + x0.rangeBand() / 2;
+      var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + height / 2;
+      x0.domain(data.map(function(d) { return d.campaign; }));
+
+      // Update the tooltip position and value
+      d3.select("#tooltip")
+        .style("top", (d3.event.pageY-50) + "px")
+        .style("left", (d3.event.pageX-100) + "px")
+        .select("#value")
+        .text(d.value);
+
+      // Show the tooltip.
+      d3.select("#tooltip").classed("hidden", false);
+    }
+
+    function hideTooltip() {
+      d3.select("#tooltip").classed("hidden", true);
+    }
+
+
     campaign.selectAll("rect")
         .data(function(d) {return d.ages;})
-
-      .enter().append("rect")
+        .enter().append("rect")
         .attr("width", x1.rangeBand())
-        .attr("x", function(d) {console.log('x1', d); return x1(d.name); })
+        .attr("x", function(d) { return x1(d.name); })
         .attr("y", function(d) { return height; })
         .attr("height", function(d) { return 0 ; })
         .attr("class","bars")
         .style("opacity",".1")
-        .on("mouseover", function(d) {
-
-            //Get this bar's x/y values, then augment for the tooltip
-            var xPosition = parseFloat(d3.select(this).attr("x")) + x0.rangeBand() / 2;
-            var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + height / 2;
-
-
-            //Update the tooltip position and value
-            d3.select("#tooltip")
-              .style("top", (d3.event.pageY-50) + "px")
-            .style("left", (d3.event.pageX-100) + "px")
-
-
-              .select("#value")
-              .text(d.value);
-
-            //Show the tooltip
-            d3.select("#tooltip").classed("hidden", false);
-
-
-           })
-           .on("mouseout", function() {
-
-            //Hide the tooltip
-            d3.select("#tooltip").classed("hidden", true)
-
-           });
-
-
-
+        .on("mouseover", showTooltip)
+        .on("mouseout", hideTooltip);
 
     campaign.selectAll("rect")
-
-
-
       .transition()
       .ease("linear")
-
       .duration(1000)
-        .attr("width", x1.rangeBand())
-        .attr("x", function(d) { console.log('x', d); return x1(d.name); })
-        .attr("y", function(d) { console.log('y', d); return y(d.value); })
-        .attr("height", function(d) { console.log('h', d); return height - y(d.value); })
-        .attr("class","bars")
-        .style("fill", function(d) { return color(d.name); })
-        .style("opacity",".7")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d) {return x1(d.name); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("class","bars")
+      .style("fill", function(d) { return color(d.name); })
+      .style("opacity",".7")
 
 
    var legend = svg.selectAll(".legend")
