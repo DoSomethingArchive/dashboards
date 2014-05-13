2d1efde00217ba8f48e81c78f48ec152b1adbcc8
Andrea Gaither <angaither@gmail.com>
2014-05-13 11:14:49 -0400

    Moved all groupbars js into a new file. - created an ajax call to fetch the data - updated tabs/etc

--- flasksite/views.py
+++ flasksite/views.py
@@ -1,23 +1,11 @@
-from flasksite import app, openDB
+from flasksite import app, openDB, json
 from flask import render_template
 
 @app.route('/')
 def home():
   return render_template('home.html')
 
-@app.route('/about')
-def about():
-  #results = overall.query.all()
-  results = 'hi'
 
-
-  return render_template('about.html',results=results)
-
-
-@app.route('/content_page_1')
-def content_page_1():
-  results = 'hi'
-  return render_template('content_page_1.html')
 
 @app.route('/morebars')
 def morebars():
@@ -30,49 +18,18 @@ def morebars():
   return render_template('morebars.html',results=results,tmp = tmp )
 
 
-
-
 @app.route('/groupbars')
 def groupbars():
-  #results = overall.query.all()
-  #tmp = []
-  #for i in results:
-    #tmp.append(int(i.sign_ups))
+  return render_template('groupbars.html')
 
+@app.route('/get-groupbars-data.json')
+def getGroupBarsData():
   cur = openDB()
   cur.execute('select campaign, sign_ups, new_members, report_backs from overall.overall where staff_pick = "y" and date_add(end_date, interval 7 day) >= curdate() order by sign_ups desc')
-  predata = []
-  for i in cur.fetchall():
-    x = {}
-    for a in i:
-      if a == 'campaign':
-        x[a]=i[a]
-      if a == 'sign_ups':
-        x['Sign Ups']=i[a]
-      if a == 'new_members':
-        x['New Members']=i[a]
-      if a == 'report_backs':
-        x['Reportbacks']=i[a]
-
-    predata.append(x)
-  data = sorted(predata,reverse=True, key=lambda k: k['Sign Ups'])
-  """
-  for i in cur.fetchall():
-    campaign = {}
-
-    for x in i:
-      campaign['State']=[0],
-      campaign['Sign Ups']=i[1],
-      campaign['New members']=i[2],
-      campaign['Reportbacks']=i[3]
-      data.append(campaign)
-      print campaign
-
-  """
-  print data
+  data = cur.fetchall()
   cur.close()
+  return json.dumps(data)
 
-  return render_template('groupbars.html', data=data )
 
 @app.route('/groupBarsNonStaff')
 def groupBarsNonStaff():
@@ -271,62 +228,6 @@ def members():
 
   return render_template('members.html', data=data )
 
-
-@app.route('/members_12')
-def members_12():
-  #results = overall.query.all()
-  #tmp = []
-  #for i in results:
-    #tmp.append(int(i.sign_ups))
-
-
-
-  return render_template('members_12.html' )
-
-@app.route('/test_submit')
-def form():
-
-
-
-
-  return render_template('test_submit.html' )
-
-
-@app.route('/test_action', methods=['POST'])
-def test_action():
-
-  try:
-    c1=request.form['c1']
-    print c1
-    c2=request.form['c2']
-    print c2
-    metric = request.form['metric']
-    print metric
-    c1_query = 'select * from overall.overall where campaign="{0}"'.format(c1)
-    cur = openDB()
-    cur.execute(c1_query)
-
-    c1_su =  cur.fetchone()[metric]
-    cur.close()
-
-    c2_query = 'select * from overall.overall where campaign="{0}"'.format(c2)
-    cur = openDB()
-    cur.execute(c2_query)
-
-    c2_su =  cur.fetchone()[metric]
-    cur.close()
-
-    return render_template('test_action.html', c1=c1,c1_su=c1_su,c2=c2,c2_su=c2_su )
-  except:
-    return render_template('query_error.html')
-
-@app.route('/test_sort')
-def test_sort():
-
-
-
-
-  return render_template('test_sort.html' )
 
 @app.route('/sortbars', methods=['POST'])
 def sortbars():
