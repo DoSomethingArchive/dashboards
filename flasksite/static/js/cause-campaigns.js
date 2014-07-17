d8c1736cad845dfd3b8be7c9d53602791928673e
Andrea Gaither <angaither@gmail.com>
2014-07-17 10:50:33 -0400

    Revert "Cssupdates2"

--- flasksite/static/js/cause-campaigns.js
+++ flasksite/static/js/cause-campaigns.js
@@ -33,11 +33,12 @@ var yAxis = d3.svg.axis()
 
 
 //append svg to the form, not the input element
-var svg = d3.select("div.main")
+var svg = d3.select("body")
+          .attr("id","campaigns")
           .append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
-          .attr("class","svgMain");
+          .attr("id","svgMain");
 
 //get specific key names
 var metrics = d3.keys(data[0]).filter(function(key) { return key !== "campaign"; });
--- flasksite/static/js/causes.js
+++ flasksite/static/js/causes.js
@@ -5,7 +5,7 @@ d3.json("/get-causes.json", function(error, json) {
 
   var div = d3.select("body")
               .append("div")
-              .attr("class", "main")
+              .attr("class", "grid")
 
 
   div.selectAll('div')
