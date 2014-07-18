14975030c767787ffa697bb9a8cc63093208450e
Andrea Gaither <angaither@gmail.com>
2014-07-18 12:51:51 -0400

    Cause specific page updates.

--- flasksite/static/js/causes.js
+++ flasksite/static/js/causes.js
@@ -1,31 +1,38 @@
 d3.json("/get-causes.json", function(error, json) {
   if (error) { return console.warn(error); }
   var data = json;
-  var format = d3.format("0,000");
 
-  var div = d3.select("body")
+  var div = d3.select("#content")
               .append("div")
               .attr("class", "main")
 
+  var causeSpace = div.selectAll("div")
+                      .data(data)
+                      .enter()
+                      .append("div")
+                      .attr("class", "cause-space");
 
-  div.selectAll('div')
-              .data(data)
-              .enter()
-              .append('button')
-              .attr("class", "front")
-              .attr("name","button")
-              .attr("type","submit")
-              .attr("value",function(d){return d.all_causes + "|" + d.cause + "|" + "y";})
-              .append("p")
-              .attr("id", "title")
-              .text(function(d){return d.cause;})
-              .append("p")
-              .attr("class", "text")
-              .on("click", getCauseCampaigns)
-              .html(function(d){return "Sign Ups : " + format(d.sign_ups)+ "</br>" + "New Members : " + format(d.new_members) + "</br>" + "Reportbacks : "+ format(d.report_backs) + "</br>" + "Traffic : "+ format(d.traffic) + "</br>" + "Gate Conversion : " + format(d.conv) + '%' + "</br>" + "Campaigns : " + format(d.campaigns);});
+  causeSpace.html(formatCampaignName);
 
+  causeSpace.append("p").html(formatCampaigninfo);
+
+  causeSpace.on("click", getCauseCampaigns);
 
 });
+
+function formatCampaignName(d) {
+  return "<h3>" + d.cause + "</h3>";
+}
+
+function formatCampaigninfo(d) {
+  var format = d3.format("0,000");
+  return "Sign Ups : " + format(d.sign_ups) + "</br>"
+        + "New Members : " + format(d.new_members) + "</br>"
+        + "Reportbacks : "+ format(d.report_backs) + "</br>"
+        + "Traffic : "+ format(d.traffic) + "</br>"
+        + "Gate Conversion : " + format(d.conv) + '%' + "</br>"
+        + "Campaigns : " + format(d.campaigns);
+}
 
 function getCauseCampaigns(d) {
   var cause_name = d.cause.toLowerCase();
