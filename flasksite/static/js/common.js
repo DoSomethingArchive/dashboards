0221acd091c7ae93a91342ac993614455a27af20
srikarg <srikarvgudipati@gmail.com>
2014-07-31 11:31:28 -0400

    Contain helper functions in ds object to avoid method name conflicts in the future.

--- flasksite/static/js/common.js
+++ flasksite/static/js/common.js
@@ -1,4 +1,7 @@
-var convertDate = function(date) {
+var ds = {};
+
+ds.convertDate = function(date) {
     var converted = new Date(date);
-    return converted.setTime(converted.getTime() + converted.getTimezoneOffset() * 60 * 1000);
+    converted.setTime(converted.getTime() + converted.getTimezoneOffset() * 60 * 1000);
+    return converted;
 };
