0cf78cb801a1f16e87d6377c8c989553a0d9364b
Josh Cusano <joshcusano@gmail.com>
2014-08-28 15:47:03 -0400

    adding queryToData function

--- flasksite/__init__.py
+++ flasksite/__init__.py
@@ -21,6 +21,30 @@ def openDB():
   cur = db.cursor()
   return cur
 
+def queryToData(cursor_obj,query,index=None,keyname=None,need_json=None):
+
+	if index==None and keyname==None and need_json==None:
+
+		cursor_obj.execute(query)
+		data = cursor_obj.fetchall()
+		data_f = json.dumps(data)
+
+		return data_f
+
+	if index!=None and keyname!=None and need_json==None:
+
+		cursor_obj.execute(query)
+		data = cursor_obj.fetchall()[index][keyname]
+		data_f = json.dumps(data)
+
+		return data_f
+
+	if index==None and keyname==None and need_json!=None:
+
+		cursor_obj.execute(query)
+		data = cursor_obj.fetchall()
+
+		return data
 
 import views
 if __name__ == '__main__':
