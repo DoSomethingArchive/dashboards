6c62c9cf8a402bb24b1f12b47b7684162017b3e5
Josh Cusano <joshcusano@gmail.com>
2014-10-01 18:07:12 -0400

    formatting, and var for chart title

--- flasksite/static/js/kpi-page.js
+++ flasksite/static/js/kpi-page.js
@@ -1,77 +1,73 @@
-//in css, clor last bar different, and make stats horizontal to eachother, color box by positive, negative
-
-function kpiChart(data,metric,color,chart_svg){
-	this.input_data=data;
-	this.metric=metric;
-	this.master_array=[{key:'Active Members',values:[],color:color}];
-	this.averages={total:0,average_last_month:0,this_month_average:0};
-	this.compare_all = 0;
-	this.compare_last_month=0;
-	this.chart_svg=chart_svg;
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-kpiChart.prototype.getShape= function () {
-	//set obj for all average stats
-	for (var i=0; i<this.input_data.length;i++)  {
-	temp_obj={};
-	temp_obj.x=this.input_data[i].date;
-	temp_obj.y=this.input_data[i][this.metric]*this.input_data[i].days_in_month;
-	//console.log(temp_obj);
-	this.master_array[0].values.push(temp_obj);
+//all values set to 0 get reassigned values in further functions
+function kpiChart(data, metric, color, chart_svg) {
+  this.input_data = data;
+  this.metric = metric;
+  this.key_name = this.metric.slice(8).capitalize() + ' Members';
+  this.master_array = [{key: this.key_name, values: [], color: color}];
+  this.averages = {total: 0, average_last_month: 0, this_month_average: 0};
+  this.compare_all = 0;
+  this.compare_last_month = 0;
+  this.chart_svg = chart_svg;
+}
+
+kpiChart.prototype.getShape = function () {
+  for (var i = 0; i<this.input_data.length; i++) {
+  temp_obj = {};
+  temp_obj.x = this.input_data[i].date;
+  temp_obj.y = this.input_data[i][this.metric] * this.input_data[i].days_in_month;
+  this.master_array[0].values.push(temp_obj);
 	}
 }
 
 kpiChart.prototype.getAverages = function () {
-	//set obj for all average stats
-	for (var i=0; i<this.input_data.length;i++)  {
-
-	if (i<this.input_data.length-1) {
-		this.averages.total+=this.input_data[i][this.metric];
-
-		if (i===this.input_data.length-2){
-		this.averages.average_last_month=this.input_data[i][this.metric];
+	for (var i=0; i<this.input_data.length;i++) {
+    //first if statement gets averages for all months except the current
+    if (i<this.input_data.length-1) {
+		  this.averages.total+=this.input_data[i][this.metric];
+      //second if statement gets the average for the last month
+		  if (i === this.input_data.length-2) {
+		    this.averages.average_last_month = this.input_data[i][this.metric];
+      }
+	  }
+    //gets the average for the current month
+    else {
+      this.averages.this_month_average = this.input_data[i][this.metric];
 		}
 	}
-
-	else {
-		this.averages.this_month_average=this.input_data[i][this.metric];
-		}
-	}
-		this.compare_all = Math.round(
-			(this.averages.this_month_average/(this.averages.total/(this.input_data.length-1))-1)
-			*100);
-		this.compare_last_month = Math.round(
-			((this.averages.this_month_average/this.averages.average_last_month)-1)
-			*100);
+  //compares the current month average to the average of all previous months
+  this.compare_all = Math.round(
+	  (this.averages.this_month_average/(this.averages.total/(this.input_data.length-1))-1) *100);
+	//compares the current month average to the last month average
+  this.compare_last_month = Math.round(((this.averages.this_month_average/this.averages.average_last_month)-1) *100);
 }
 
-kpiChart.prototype.buildChart=ds.makeMyChart;
+kpiChart.prototype.buildChart = ds.makeMyChart;
 
 kpiChart.prototype.addStatsToPage= function () {
-	var last_month=this.metric+'_last_month';
-	var all =this.metric+'_all';
+  var last_month = this.metric + '_last_month';
+  var all = this.metric + '_all';
 	document.getElementById(last_month)
-		.innerHTML = 'Pecent change, last month average: '+this.compare_last_month.toString()+'%';
+		.innerHTML = 'Pecent change, last month average: ' + this.compare_last_month.toString() + '%';
 	document.getElementById(all)
-		.innerHTML = 'Pecent change, year average: '+this.compare_all.toString()+'%';
-
+		.innerHTML = 'Pecent change, year average: ' + this.compare_all.toString() + '%';
 }
 
 kpiChart.prototype.colorBar = function(c) {
-
-	var chart_string = c.slice(0, -4);
-	var chart_rect = chart_string+' rect';
-
- 	var checkExist = setInterval(function() {
-
+  var chart_string = c.slice(0, -4);
+  var chart_rect = chart_string +' rect';
+  var checkExist = setInterval( function () {
+    //checks if the rect element exists yet
     if ($(chart_rect).length) {
       $chart = $(chart_string);
- 			$lastRect = $chart.find('rect:last-child');
- 			$lastRect.css({'fill':'#FFCC33'});
-
+ 		  $lastRect = $chart.find('rect:last-child');
+ 		  $lastRect.css({'fill':'#FFCC33'});
       clearInterval(checkExist);
-    	}
-    }, 10);
+    }
+  }, 10);
 }
 
 //stats for active
@@ -97,12 +93,3 @@ new_m.getAverages();
 new_m.buildChart(new_m.chart_svg,new_m.master_array);
 new_m.addStatsToPage();
 new_m.colorBar(new_m.chart_svg);
-
-
-
-
-
-
-
-
-
