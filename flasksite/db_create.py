189d7e840acb1b69c32025c770bb61a504f96024
Josh Cusano <joshcusano@gmail.com>
2015-03-19 15:53:50 -0400

    adding script to create sqlalchemy db

---
+++ flasksite/db_create.py
@@ -0,0 +1,15 @@
+import sys
+#Run to create sqlalchemy for the first time
+#set so can import config vars
+sys.path.insert(0, '/users/dosomething/git/dashboards/env')
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from flasksite import db
+import os.path
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
